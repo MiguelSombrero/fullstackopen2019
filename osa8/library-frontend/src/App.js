77b8fffb8ddc9b73d497989b89b7b934240cce7a
Miika Somero <miika.somero@gmail.com>
2019-08-19 08:59:55 +0300

    tehtävät 8.18 saakka (ei 8.12)

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -3,7 +3,8 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { gql } from 'apollo-boost'
-import { useQuery, useMutation } from '@apollo/react-hooks'
+import { useQuery, useMutation, useApolloClient } from '@apollo/react-hooks'
+import LoginForm from './components/LoginForm'
 
 const ALL_AUTHORS = gql`
 {
@@ -15,18 +16,18 @@ const ALL_AUTHORS = gql`
   }
 }
 `
-
 const ALL_BOOKS = gql`
 {
   allBooks {
     title
-    author
+    author {
+      name
+    }
     published
     id
   }
 }
 `
-
 const ADD_BOOK = gql`
   mutation addBook($title: String!, $published: Int!, $author: String!, $genres: [String]!) {
     addBook(
@@ -37,13 +38,14 @@ const ADD_BOOK = gql`
     ) {
       title
       published
-      author
+      author {
+        name
+      }
       genres
       id
     }
   }
 `
-
 const EDIT_AUTHOR = gql`
   mutation editAuthor($name: String!, $born: Int!) {
     editAuthor(name: $name, setBornTo: $born) {
@@ -53,9 +55,28 @@ const EDIT_AUTHOR = gql`
     }
   }
 `
+const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
+    }
+  }
+`
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState('')
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+
+  const client = useApolloClient()
+
+  const handleError = (error) => {
+    setErrorMessage(error.graphQLErrors[0].message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   const authors = useQuery(ALL_AUTHORS)
   const books = useQuery(ALL_BOOKS)
 
@@ -67,13 +88,43 @@ const App = () => {
     refetchQueries: [{ query: ALL_AUTHORS }]
   })
 
+  const [login] = useMutation(LOGIN, {
+    onError: handleError
+  })
+
+  const errorNotification = () => errorMessage &&
+    <div style={{ color: 'red '}}>
+      {errorMessage}
+    </div>
+
+  const logout = () => {
+    setToken(null)
+    window.localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        {errorNotification()}
+        <h2>Login</h2>
+        <LoginForm
+          login={login}
+          setToken={(token) => setToken(token)}
+        /> 
+      </div>
+    )
+  }
+
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => logout()}>Logout</button>
       </div>
+      {errorNotification()}
 
       <Authors
         show={page === 'authors'}
